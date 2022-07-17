418ea398ebe83b139b0e31a94cdd56516845f683
github_username <princekumarsharma365@gmail.com>
2022-07-17 17:13:20 +0530

    furniture project designing

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const path = require('path');
+//const { send } = require('process');
+
+const app = express();
+
+app.use(express.json());
+
+app.use(express.static(path.join(__dirname, 'client')))
+    .get('/', (request, response) => response.render('furnitureIndex.html'));
+
+
+app.listen(4000,function(){
+        console.log('Server is running on port 4000')
+    });
+
+
