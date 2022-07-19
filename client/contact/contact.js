3d5f2ad21549747458785e4000b20a23c364c716
github_username <princekumarsharma365@gmail.com>
2022-07-19 17:04:10 +0530

    furniture store project ui

--- client/contact/contact.js
+++ client/contact/contact.js
@@ -4,9 +4,17 @@ function submitForm(){
    var email = document.getElementById('email').value;
    var phone = document.getElementById('phone').value;
    var query = document.getElementById('query').value;
+
+   const customer={
+    name:name,
+    email:email,
+    phone:phone,
+    query:query,
+   };
    if(name && email && phone && query ) {
-   console.log('send data to server' ,name,email,phone,query);
+   console.log('send data to server' ,customer);
    } else {
    alert('Please provide full detail')
   }
+
 }  
