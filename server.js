be3e896438c98342c1b41dfa25f4b8f20be1fd9d
Rajeev Sharma <talk2rajeev1986@gmail.com>
2022-07-27 18:31:44 +0530

    add2cart qty issue

--- server.js
+++ server.js
@@ -13,131 +13,132 @@ app.use(express.static(path.join(__dirname, 'client')))
     .get('/', (request, response) => response.render('index.html'));
 
 
-    const productList=[
-        {
-            id:'1',
-            name:'wooden bed',
-            price: '39,999',
-            originalPrice: '59,999',
-            image:'bed4.webp',
-        },
-        {
-            id:'2',
-            name:' king wooden bed ',
-            price: '49,999',
-            originalPrice: '69,999',
-            image:'bed3.webp',
-        },
-        {
-            id:'3',
-            name:' queen wooden bed ',
-            price: '29,999',
-            originalPrice: '49,999',
-            image:'bed1.webp',
-        },
-        {
-            id:'4',
-            name:' queen wooden bed ',
-            price: '25,999',
-            originalPrice: '49,999',
-            image:'bed6.jpg',
-        },
-        {
-            id:'5',
-            name:' Teak wooden bed ',
-            price: '45,999',
-            originalPrice: '49,999',
-            image:'bed5.webp',
-        },
-        {
-            id:'6',
-            name:' Teak wooden bed ',
-            price: '12,999',
-            originalPrice: '19,999',
-            image:'bed7.jpg',
-        },
-        {
-            id:'7',
-            name:'  wooden bed ',
-            price: '25,999',
-            originalPrice: '39,999',
-            image:'bed2.webp',
-        },
-        {
-            id:'5',
-            name:' Teak wooden bed ',
-            price: '45,999',
-            originalPrice: '49,999',
-            image:'bed5.webp',
-        },
-    ];
+const productList = [
+    {
+        id: '1',
+        name: 'wooden bed',
+        price: '39,999',
+        originalPrice: '59,999',
+        image: 'bed4.webp',
+    },
+    {
+        id: '2',
+        name: ' king wooden bed ',
+        price: '49,999',
+        originalPrice: '69,999',
+        image: 'bed3.webp',
+    },
+    {
+        id: '3',
+        name: ' queen wooden bed ',
+        price: '29,999',
+        originalPrice: '49,999',
+        image: 'bed1.webp',
+    },
+    {
+        id: '4',
+        name: ' queen wooden bed ',
+        price: '25,999',
+        originalPrice: '49,999',
+        image: 'bed6.jpg',
+    },
+    {
+        id: '5',
+        name: ' Teak wooden bed ',
+        price: '45,999',
+        originalPrice: '49,999',
+        image: 'bed5.webp',
+    },
+    {
+        id: '6',
+        name: ' Teak wooden bed ',
+        price: '12,999',
+        originalPrice: '19,999',
+        image: 'bed7.jpg',
+    },
+    {
+        id: '7',
+        name: '  wooden bed ',
+        price: '25,999',
+        originalPrice: '39,999',
+        image: 'bed2.webp',
+    },
+    {
+        id: '5',
+        name: ' Teak wooden bed ',
+        price: '45,999',
+        originalPrice: '49,999',
+        image: 'bed5.webp',
+    },
+];
 
-    app.get('/getProductList',(request,response)=>{
-        response.send(productList);
-        
-    });
+app.get('/getProductList', (request, response) => {
+    response.send(productList);
 
-    app.post('/gettingProduct',(request,response)=>{
-        const product=request.body;
-        console.log(productList.length);
-        productList.push(product);
-        response.send({msg:'product added successfully'})
-    });
+});
 
-    let cart = [];
+app.post('/gettingProduct', (request, response) => {
+    const product = request.body;
+    console.log(productList.length);
+    productList.push(product);
+    response.send({ msg: 'product added successfully' })
+});
 
-    app.get('/getCart',(request, response)=>{
-      
-        response.send(cart);
-    });
 
-    app.post('/addToCart', (request, response) =>{
-        const cartItem = request.body;
-        // if item not present in cart make quantity =1
-        // if item already present in cart increase quantity by 1
-        let itemPresent=true;
-        let quantity=[];
 
-        for(i=0;i<productList.length;i++){
-            if(productList[i].id!==cartItem.id){
-                itemPresent=false;
-                //console.log('item not Present')
-            }
-            cart.push(cartItem);
+app.get('/getCart', (request, response) => {
 
-            if(itemPresent===true){
-            //response.send={msg:'item Already Present'}
-            if(quantity[productList[i]]){
-                quantity[productList[i]]+=1;
-            }
-            cart.push(quantity);
-        }   
-        else
+    response.send(cart);
+});
+
+let cart = [];
+app.post('/addToCart', (request, response) => {
+    const cartItem = request.body;
+    // if item not present in cart make quantity =1
+    // if item already present in cart increase quantity by 1
+    let index = -1;
+
+    for (i = 0; i < cart.length; i++) {
+        if (cart[i].id === cartItem.id) {
+            index = i;
+            //console.log('item not Present')
         }
+    }
+    if(index !== -1) {
+        cart[index].qty =  cart[index].qty +1;
+    } else {
+        var cartWithQty = {
+            id: cartItem.id,
+            name: cartItem.productName,
+            price: cartItem.price,
+            img: cartItem.img,
+            qty: 1
+        }
+        cart.push(cartWithQty);
+    }
 
-       // cart.push(cartItem);
-        response.send({msg: 'Item added to cart'});
-        });
-
-    
-        // delete cart items
-
-    app.delete('/cartDelete',(request,response)=>{
-        // const name=request.body; 
-        console.log("request.body", request.body);
-
-        const filteredCard = cart.filter(c => c.id !== parseInt(request.body.id));
-       console.log('filteredCard ', filteredCard);
-        cart = filteredCard;
-        response.send({msg: 'Item deleted'});
-    });
-
+    response.send(cart);
+});
 
 
+// delete cart items
+
+app.delete('/cartDelete', (request, response) => {
+    // const name=request.body; 
+    console.log("request.body", request.body);
+
+    const filteredCard = cart.filter(c => c.id !== parseInt(request.body.id));
+    console.log('filteredCard ', filteredCard);
+    cart = filteredCard;
+    response.send({ msg: 'Item deleted' });
+});
 
 
-app.listen(4000,function(){
-        console.log('Server is running on port 4000')
-    });
+
+
+
+app.listen(4000, function () {
+    console.log('Server is running on port 4000')
+});
 
 
